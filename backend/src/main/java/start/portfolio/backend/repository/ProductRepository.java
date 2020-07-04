5790106cb65ca9a5ebe1563888accfd6c1252e75
chang <chang@DESKTOP-L2KUJ51>
2020-07-04 14:08:05 +0900

    product findbyId

--- backend/src/main/java/start/portfolio/backend/repository/ProductRepository.java
+++ backend/src/main/java/start/portfolio/backend/repository/ProductRepository.java
@@ -1,10 +1,16 @@
 package start.portfolio.backend.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import start.portfolio.backend.dto.ProductDto;
 import start.portfolio.backend.entity.Product;
 
 
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
+	
+	Optional<Product> findById(Long id);
 
 }
